6cb7c4f0b2a62b30728747124c7aef64c98fc266
Jon0067 <63180126+Jon0067@users.noreply.github.com>
2020-05-13 07:43:30 +0700

    Working Data Province

--- src/components/List.js
+++ src/components/List.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import {View, Text, FlatList, StyleSheet} from 'react-native';
 
 class List extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			data:[],
+			province:[],
 			refreshing:false
 		}
+		//keyExtractorCovid=(_, index) => index.toString()
 }
 
-renderItem = ({item}) => (
-	<View style={{padding: 20, borderBottomWidth:1, borderBottomColor:'#000'}}>
-		<Text>Name : {item.name.first}</Text>
+renderItem = ({ item }) => (
+	<View style={{flex : 1 ,padding: 10, borderBottomWidth:1, borderBottomColor:'#000'}}>
+		<Text>{item.provinsi} </Text>
+		<View style={{flexDirection:'row'}}>
+			<Text style={styles.Positif}>{item.kasusPosi} </Text>
+			<Text style={styles.sembu}>{item.kasusSemb}</Text>
+			<Text style={styles.Meninggal}>{item.kasusMeni}</Text>
+		</View>
 	</View>
 )
 
@@ -26,12 +32,9 @@ componentDidMount = () => {
 
 getDataApi = async () => {
 	this.setState({refreshing:true})
-	/// fetch('http://jsonplaceholder.typicode.com/users')
-	// 	.then(response => response.json())
-	// 	.then(json => this.setState({users: json, refreshing: false}))
-	const response = await fetch('https://api.randomuser.me/?results=5')
-	const json = await response.json();
-	this.setState({data: json, refreshing: false})
+        const response = await fetch('https://indonesia-covid-19.mathdro.id/api/provinsi')
+        const json = await response.json()
+        this.setState({province: json.data,refreshing:false})
 }
 
 render(){
@@ -39,8 +42,8 @@ render(){
 		return(
 			<View>
 				<FlatList
-					data={this.state.data}
-					keyExtractor={item => item.login.uuid.toString()}
+					data={this.state.province}
+                    keyExtractor={({provinsi}, index) => provinsi}
 					renderItem={this.renderItem}
 					refreshing={this.state.refreshing}
 					onRefresh={()=> { }}
@@ -49,5 +52,20 @@ render(){
 		)
 	}
 }
+
+const styles = StyleSheet.create({
+  sembu: {
+    backgroundColor:'limegreen',
+    width:45
+  },
+  Positif:{
+    backgroundColor:'orange',
+    width:45
+  },
+  Meninggal:{
+    backgroundColor:'gray',
+    width:45
+  },
+});
 
 export default List;
